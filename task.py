b90c41a87e75d3a19b664ae9b8e4800bb30510d0
obich <g.shchet@gmail.com>
2024-03-06 23:08:14 +0300

    6 task

--- task.py
+++ task.py
@@ -2,10 +2,9 @@ from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/promotion_image')
-def return_mars_page():
-    return render_template('index.html')
+@app.route('/choice/<planet_name>')
+def return_mars_page(planet_name):
+    return render_template('index.html', planet_name=planet_name)
 
 
 if __name__ == '__main__':
